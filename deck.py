c40b062578b045892e23c6336326c5ef9328acf3
Zhao-Mingyang <13862009177@163.com>
2022-09-21 11:32:28 +0800

    changes

--- deck.py
+++ deck.py
@@ -3,24 +3,25 @@ import numpy as np
 
 
 class Deck(object):
-    def __init__(self, num_deck=1):
+    def __init__(self, num_deck=1, is_black_jack = True):
         # num_decks = 0 means infinite number of decks and the pick up is with replacement
-        self.contents = [(2, 'Diamonds'), (2, 'Hearts'), '2 of Clubs', '2 of Spades', (3, 'Diamonds'), (3, 'Hearts'),\
-                         '3 of Clubs', '3 of Spades', (4, 'Diamonds'), (4, 'Hearts'), '4 of Clubs', '4 of Spades',\
-                         (5, 'Diamonds'), (5, 'Hearts'), '5 of Clubs', '5 of Spades', (6, 'Diamonds'), (6, 'Hearts'),\
-                         '6 of Clubs', '6 of Spades', (7, 'Diamonds'), (7, 'Hearts'), '7 of Clubs', '7 of Spades',\
-                         (8, 'Diamonds'), (8, 'Hearts'), '8 of Clubs', '8 of Spades', (9, 'Diamonds'), (9, 'Hearts'),\
-                         '9 of Clubs', '9 of Spades', (10, 'Diamonds'), (10, 'Hearts'), '10 of Clubs', '10 of Spades',\
-                         (11, 'Diamonds'), (11, 'Hearts'), 'Jack of Clubs', 'Jack of Spades', (12, 'Diamonds'),\
-                         (12, 'Hearts'), 'Queen of Clubs', 'Queen of Spades', (13, 'Diamonds'), (13, 'Hearts'),\
-                         'King of Clubs', 'King of Spades', (1, 'Diamonds'), (1, 'Hearts'), 'Ace of Clubs',\
-                         'Ace of Spades']
+        self.contents = [(2, 'Diamonds'), (2, 'Hearts'), (2, 'Clubs'), (2, 'Spades'), (3, 'Diamonds'), (3, 'Hearts'),\
+                         (3, 'Clubs'), (3, 'Spades'), (4, 'Diamonds'), (4, 'Hearts'), (4, 'Clubs'), (4, 'Spades'),\
+                         (5, 'Diamonds'), (5, 'Hearts'), (5, 'Clubs'), (5, 'Spades'), (6, 'Diamonds'), (6, 'Hearts'),\
+                         (6, 'Clubs'), (6, 'Spades'), (7, 'Diamonds'), (7, 'Hearts'), (2, 'Clubs'), '7 of Spades',\
+                         (8, 'Diamonds'), (8, 'Hearts'), (8, 'Clubs'), (8, 'Spades'), (9, 'Diamonds'), (9, 'Hearts'),\
+                         (9, 'Clubs'), (9, 'Spades'), (10, 'Diamonds'), (10, 'Hearts'), (10, 'Clubs'), (10, 'Spades'),\
+                         (11, 'Diamonds'), (11, 'Hearts'), (11, 'Clubs'), (11, 'Spades'), (12, 'Diamonds'),\
+                         (12, 'Hearts'), (12, 'Clubs'), (12, 'Spades'), (13, 'Diamonds'), (13, 'Hearts'),\
+                         (13, 'Clubs'), (13, 'Spades'), (1, 'Diamonds'), (1, 'Hearts'), (1, 'Clubs'),\
+                         (1, 'Spades')]
         if num_deck is not 0:
             self.decks = [self.contents for _ in range(num_deck)]
             self.decks = list(np.concatenate(self.decks, axis=0))
         else:
             self.decks = list(self.contents)
         self.num_deck = num_deck
+        self.is_black_jack = is_black_jack
 
     def shuffle(self):
         # shuffle the cards
@@ -29,6 +30,8 @@ class Deck(object):
     def pop(self):
         # pop the top card out and hand it to the player
         card = self.decks.pop(0)
+        if self.is_black_jack and card[0]>10:
+            card[0] = 10
         # for num_deck = 0, pick up with replacement
         if self.num_deck == 0:
             self.decks = list(self.contents)
